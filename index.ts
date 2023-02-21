0ba4ae5a2f76df07da0a69e543da83ce35b170dd
Anton <anymy9813@gmail.com>
2023-02-21 13:26:35 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT
+
+app.get("/", (req,res) => {res.send("success")})
+
+app.listen(PORT, () => {
+  console.log("Server has been started on port " + PORT); 
+})
